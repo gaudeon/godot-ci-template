6dcc81c748f162e3d998b59cf981c4134adb53c7
Travis Chase <travis.chase@gmail.com>
2021-12-24 11:43:21 -0700

    added Makefile to locally run unit tests without redownloading godot headless each time update gut.sh to later version of godot headless and made it compatible with makefile updated .gitignore to ignore hidden .godot directory (where godot headless is now downloaded instead of root dir)

--- gut.sh
+++ gut.sh
@@ -11,8 +11,9 @@ echo "░ ░   ░  ░░░ ░ ░   ░           ░         ░   ░  �
 echo "      ░    ░                             ░  ░      ░                 ░  ";
 echo "                                                                        ";
 
-LANG="en_US.utf8"
-GODOT_VERSION=3.2.2
+LANG=en_US.utf8
+GODOT_VERSION=3.4
+GODOT_DIR=.godot
 GODOT_BIN=Godot_v${GODOT_VERSION}-stable_linux_headless.64
 GODOT_ZIP=${GODOT_BIN}.zip
 GODOT_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/${GODOT_ZIP}
@@ -21,7 +22,8 @@ if [[ -z "${GITHUB_WORKSPACE}" ]]; then
   GITHUB_WORKSPACE=$PWD
 fi
 
-curl -s -S ${GODOT_URL} --output ${GODOT_ZIP}
-unzip -q ${GODOT_ZIP}
-chmod +x ${GODOT_BIN}
-./${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
+mkdir -p ${GODOT_DIR}
+curl -s -S ${GODOT_URL} --output ${GODOT_DIR}/${GODOT_ZIP}
+unzip -q -n ${GODOT_DIR}/${GODOT_ZIP} -d ${GODOT_DIR}
+chmod +x ${GODOT_DIR}/${GODOT_BIN}
+${GODOT_DIR}/${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
